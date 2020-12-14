254f9acd1b89eefbaf8a838947586a8cf2c3c28c
FoteiniRodi <60667452+FoteiniRodi@users.noreply.github.com>
2020-12-14 19:46:53 +0200

    Create IntroductionToGitAndGitHub_TableOfContents.py

---
+++ IntroductionToGitAndGitHub_TableOfContents.py
@@ -0,0 +1,109 @@
+#%%
+"""
+Introduction to Git and GitHub, by Google
+
+TABLE OF CONTENTS
+
+WEEK 1 – Introduction to Version Control
+1.1 Course Introduction
+    Course Introduction
+    Welcome to the course
+1.2 Before Version Control
+    1.2.1	 Intro to Module 1: Version Control
+    1.2.2    Keeping Historical Copies
+    1.2.3    Diffing Files
+    1.2.4    Applying Changes
+    1.2.5    Practical Application of diff and patch
+    1.2.6    diff and patch Cheat Sheet 
+    1.2.7    Practice Quiz
+1.3 Version Control Systems
+    1.3.1	What is version control?
+    1.3.2	Version Control and Automation
+    1.3.3	What is Git?
+    1.3.4	Reading: More Information About Git
+    1.3.5	Installing Git
+    1.3.6	Installing Git on Windows (Optional)
+    1.3.7	Reading: Installing Git
+    1.3.8	Practice Quiz
+1.4 Using Git
+    1.4.1	First Steps with Git
+    1.4.2   Tracking Files
+    1.4.3   The Basic Git Workflow
+    1.4.4   Anatomy of a Commit Message
+    1.4.5   Reading: Initial Git Cheat Sheet
+    1.4.6   Practice Quiz
+1.5 Qwiklabs Assessment: Introduction to Git
+
+WEEK 2 – Using Git Locally
+2.1 Advanced Git interaction
+	2.1.1 Intro to Module 2: Using Git Locally
+	2.1.2 Skipping the Staging Area
+	2.1.3 Getting More Information About Our Changes
+	2.1.4 Deleting and Renaming Files
+	2.1.5 Advanced Git Cheat Sheet
+	2.1.6 Practice Quiz
+2.2 Undoing things
+    2.2.1 Undoing Changes Before Committing	
+    2.2.2 Amending Commits
+    2.2.3 Rollbacks
+    2.2.4 Identifying a Commit
+    2.2.5 Git Revert Cheat Sheet
+    2.2.6 Practice Quiz
+2.3 Branching and Merging
+    2.3.1 What is a branch?
+    2.3.2 Creating New Branches
+    2.3.3 Working with Branches
+    2.3.4 Merging
+    2.3.5 Merge Conflicts
+    2.3.6 Git Branches and Merging Cheat Sheet
+    2.3.7 Practice Quiz
+2.4 Qwiklabs Assessment: Merging Branches in Git
+
+WEEK 3 - Working with Remotes
+3.1 Introduction to GitHub
+    3.1.1 Intro to Module 3: Working with Remotes
+    3.1.2 What is GitHub?
+    3.1.3 Basic Interaction with GitHub
+    3.1.4 Basic Interaction with GitHub Cheat-Sheet
+    3.1.5 Practice Quiz
+3.2 Using a Remote Repository
+    3.2.1 What is a remote?
+    3.2.2 Working with Remotes
+    3.2.3 Fetching New Changes
+    3.2.4 Updating the Local Repository
+    3.2.5 Git Remotes Cheat-Sheet
+    3.2.6 Practice Quiz
+3.3 Solving Conflicts
+    3.3.1 The Pull-Merge-Push Workflow
+    3.3.2 Pushing Remote Branches
+    3.3.3 Rebasing Your Changes
+    3.3.4 Another Rebasing Example
+    3.3.5 Best Practices for Collaboration
+    3.3.6 Conflict Resolution Cheat Sheet
+    3.3.7 Practice Quiz
+3.4 Qwiklabs Assessment: Introduction to Github
+
+WEEK 4 - Collaboration
+4.1 Pull Requests
+    4.1.1 Intro to Module 4: Collaboration
+    4.1.2 A Simple Pull Request on GitHub
+    4.1.3 The Typical Pull Request Workflow on GitHub
+    4.1.4 Updating an Existing Pull Request
+    4.1.5 Squashing Changes
+    4.1.6 Git Fork and Pull Request Cheat Sheet
+    4.1.7 Practice Quiz
+4.2 Code Reviews
+    4.2.1 What are code reviews?
+    4.2.2 The Code Review Workflow
+    4.2.3 How to Use Code Reviews in GitHub
+    4.2.4 More Information on Code Reviews
+    4.2.5 Practice Quiz
+4.3 Managing Projects
+    4.3.1 Managing Collaboration
+    4.3.2 Tracking Issues
+    4.3.3 Continuous Integration
+    4.3.4 Additional Tools
+    4.3.5 Practice Quiz
+4.4 Qwiklabs Assessment: Pushing Local Commits to Github
+
+"""
